82d5e884321890fc2163e689ffef1c6bdf84d728
vlad <vladislavludinov3006@gmail.com>
2022-12-19 16:38:31 +0300

    search max added in Task4

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -3,3 +3,17 @@
 int a = int.Parse(ReadLine());
 int b = int.Parse(readLine());
 int c = int.Parse(ReadLine());
+
+if (a >= b && a >= c)
+{
+    max = a;
+}
+else
+    if (c >= b)
+    {
+        max = c;
+    }
+    else
+    {
+        max = b;
+    }
