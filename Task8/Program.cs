1d85f1228a4093fce89fe694aef49525edfcdfa9
vlad <vladislavludinov3006@gmail.com>
2022-12-19 17:23:47 +0300

    Task8 complete

--- Task8/Program.cs
+++ Task8/Program.cs
@@ -1,4 +1,20 @@
 ﻿using static System.Console;
 
 int number = int.Parse(ReadLine());
+int count = 1;
 
+while (count <= number)
+{
+    if (number - count > 1 && count % 2 == 0)
+    {
+        Write($"{count}, ");
+    }
+    else
+    {
+        if (count % 2 == 0)
+        {
+            Write(count);
+        }
+    }
+    count+=1;
+}
